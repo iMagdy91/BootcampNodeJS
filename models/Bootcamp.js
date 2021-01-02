89796190b8fae986e1934bd11bcbda68f8fad681
Mohamed Hassanin <magdymohamed1991@gmail.com>
2021-01-02 22:34:42 +0100

    adding bootcamps slugify package

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
 	name: {
@@ -97,4 +98,9 @@ const BootcampSchema = new mongoose.Schema({
 	}
 });
 
+// Create bootcamp slug from name
+BootcampSchema.pre('save', function (next) {
+	this.slug = slugify(this.name, { lower: true });
+	next();
+});
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
